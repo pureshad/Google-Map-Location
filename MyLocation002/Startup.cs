a8d7035115c57ede69056982b06ed6858f33b93d
IluXa T <ilyaproveril@hotmail.com>
2018-06-05 23:57:14 +0300

    Removed GeoLocalization

--- MyLocation002/Startup.cs
+++ MyLocation002/Startup.cs
@@ -33,7 +33,7 @@ namespace MyLocation002
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsProduction())
             {
                 app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
